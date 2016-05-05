fc8ca59c188b7449d34373f5b6622165287ec6c4
farhad taran <farhad taran>
2016-05-05 15:55:40 +0100

    Init

---
+++ Trains.Core.Tests/DataStructures/GraphTests/GraphTestBase.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trains.Core.DataStructures;
+
+namespace Trains.Core.Tests.DataStructures.GraphTests
+{
+    [TestClass]
+    public abstract class GraphTestBase
+    {
+        protected abstract int Size { get; }
+        protected abstract Action<Graph> Action { get; }
+        protected Graph Graph { get; private set; } 
+
+        [TestInitialize]
+        public void BaseInitialize()
+        {
+            Graph = new Graph(Size);
+            Action(Graph);
+        }
+    }
+
+    [TestClass]
+    public class WhenAddingAnEdgeBetweenVerticesOneAndTwo : GraphTestBase
+    {
+        protected override int Size => 3;
+
+        protected override Action<Graph> Action => graph => graph.AddEdge(1, 2);
+
+        [TestMethod]
+        public void ThenHasEdgeReturnsTrueForOneAndTwo()
+        {
+            Assert.IsTrue(Graph.HasEdge(1, 2));
+        }
+
+        [TestMethod]
+        public void ThenHasEdgeReturnsFalseForTwoAndThree()
+        {
+            Assert.IsFalse(Graph.HasEdge(2, 3));
+        }
+
+        [TestMethod]
+        public void ThenHasEdgeReturnsFalseForOneAndThree()
+        {
+            Assert.IsFalse(Graph.HasEdge(1, 3));
+        }
+    }
+
+    [TestClass]
+    public class WhenAddingAnEdgeBetweenVerticesTwoAndThree : GraphTestBase
+    {
+        protected override int Size => 3;
+
+        protected override Action<Graph> Action => graph => graph.AddEdge(2, 3);
+
+        [TestMethod]
+        public void ThenHasEdgeReturnsTrueForOneAndTwo()
+        {
+            Assert.IsTrue(Graph.HasEdge(2, 3));
+        }
+
+        [TestMethod]
+        public void ThenHasEdgeReturnsFalseForOneAndTwo()
+        {
+            Assert.IsFalse(Graph.HasEdge(1, 2));
+        }
+
+        [TestMethod]
+        public void ThenHasEdgeReturnsFalseForOneAndThree()
+        {
+            Assert.IsFalse(Graph.HasEdge(1, 3));
+        }
+    }
+
+    [TestClass]
+    public class WhenAddingAnEdgeBetweenVerticesOneAndThree : GraphTestBase
+    {
+        protected override int Size => 3;
+
+        protected override Action<Graph> Action => graph => graph.AddEdge(1, 3);
+
+        [TestMethod]
+        public void ThenHasEdgeReturnsTrueForOneAndThree()
+        {
+            Assert.IsTrue(Graph.HasEdge(1, 3));
+        }
+
+        [TestMethod]
+        public void ThenHasEdgeReturnsFalseForOneAndTwo()
+        {
+            Assert.IsFalse(Graph.HasEdge(1, 2));
+        }
+
+        [TestMethod]
+        public void ThenHasEdgeReturnsFalseForTwoAndThree()
+        {
+            Assert.IsFalse(Graph.HasEdge(2, 3));
+        }
+    }
+
+    [TestClass]
+    public class WhenGettingSuccessorsOfVeritxWithOneSuccessor : GraphTestBase
+    {
+        protected override Action<Graph> Action => (graph) =>
+        {
+            graph.AddEdge(1, 3);
+        };
+
+        protected override int Size => 3;
+
+        [TestMethod]
+        public void ReturnsOneSuccessor()
+        {
+            Assert.AreEqual(1, Graph.GetSuccessors(1).Count);
+            Assert.AreEqual(3, Graph.GetSuccessors(1)[0]);
+        }
+    }
+}
---
+++ Trains.Core/DataStructures/Graph.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Trains.Core.DataStructures
+{
+    /// <summary>Represents a directed unweighted graph structure
+    /// </summary>
+    public class Graph
+    {
+        // Contains the child nodes for each vertex of the graph
+        // assuming that the vertices are numbered 0 ... Size-1
+        private List<int>[] _childNodes;
+
+        /// <summary>Constructs an empty graph of given size</summary>
+        /// <param name="size">number of vertices</param>
+        public Graph(int size)
+        {
+            if (size == 0)
+                throw new ArgumentException("size needs to be larger than zero.");
+
+            _childNodes = new List<int>[size];
+            for (int i = 0; i < size; i++)
+            {
+                // Assing an empty list of adjacents for each vertex
+                _childNodes[i] = new List<int>();
+            }
+        }
+
+        /// <summary>Constructs a graph by given list of
+        /// child nodes (successors) for each vertex</summary>
+        /// <param name="childNodes">children for each node</param>
+        public Graph(List<int>[] childNodes)
+        {
+            if (childNodes.Any() == false)
+                throw new ArgumentException("child nodes cannot be empty.");
+
+            _childNodes = childNodes;
+        }
+
+        /// <summary>
+        /// Returns the size of the graph (number of vertices)
+        /// </summary>
+        public int Size
+        {
+            get { return _childNodes.Length; }
+        }
+
+        /// <summary>Adds new edge from u to v</summary>
+        /// <param name="u">the starting vertex</param>
+        /// <param name="v">the ending vertex</param>
+        public void AddEdge(int u, int v)
+        {
+            _childNodes[u].Add(v);
+        }
+
+        /// <summary>Removes the edge from u to v if such exists
+        /// </summary>
+        /// <param name="u">the starting vertex</param>
+        /// <param name="v">the ending vertex</param>
+        public void RemoveEdge(int u, int v)
+        {
+            _childNodes[u].Remove(v);
+        }
+
+        /// <summary>
+        /// Checks whether there is an edge between vertex u and v
+        /// </summary>
+        /// <param name="u">the starting vertex</param>
+        /// <param name="v">the ending vertex</param>
+        /// <returns>true if there is an edge between
+        /// vertex u and vertex v</returns>
+        public bool HasEdge(int u, int v)
+        {
+            bool hasEdge = _childNodes[u].Contains(v);
+            return hasEdge;
+        }
+
+        /// <summary>Returns the successors of a given vertex
+        /// </summary>
+        /// <param name="v">the vertex</param>
+        /// <returns>list of all successors of vertex v</returns>
+        public IList<int> GetSuccessors(int v)
+        {
+            return _childNodes[v];
+        }
+    }
+}
