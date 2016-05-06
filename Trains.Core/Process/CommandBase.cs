18bd35602ca5ddb67e5baa300f8d1f89ba1c3e33
Farhad Taran <farhad86@gmail.com>
2016-05-06 07:30:25 +0100

    wip

---
+++ Trains.Core/Process/CommandBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trains.Core.DataStructures;
+
+namespace Trains.Core.Process
+{
+    public abstract class OperationBase
+    {
+        public abstract string Instructions { get; }
+    }
+
+    public abstract class CommandBase : OperationBase
+    {
+        public abstract void process(string input, Graph<char> graph);
+    }
+
+    public abstract class QueryBase : OperationBase
+    {
+        public abstract string process(string input, Graph<char> graph);
+    }
+
+}
