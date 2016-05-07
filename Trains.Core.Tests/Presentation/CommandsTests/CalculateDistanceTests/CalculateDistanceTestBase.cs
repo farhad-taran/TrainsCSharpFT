eb6d70e01526537f5bde5c263322a0883690cffb
Farhad Taran <farhad86@gmail.com>
2016-05-07 18:55:12 +0100

    wip

--- Trains.Core.Tests/Presentation/CommandsTests/CalculateDistanceTests/CalculateDistanceTestBase.cs
+++ Trains.Core.Tests/Presentation/CommandsTests/CalculateDistanceTests/CalculateDistanceTestBase.cs
@@ -67,11 +67,22 @@ namespace Trains.Core.Tests.Presentation.CommandsTests.CalculateDistanceTests
             var a = new GraphNode<char>('A');
             var b = new GraphNode<char>('B');
             var c = new GraphNode<char>('C');
+            var d = new GraphNode<char>('D');
+            var e = new GraphNode<char>('E');
             Graph.AddNode(a);
             Graph.AddNode(b);
             Graph.AddNode(c);
+            Graph.AddNode(d);
+            Graph.AddNode(e);
             Graph.AddDirectedEdge(a, b, 5);
             Graph.AddDirectedEdge(b, c, 4);
+            Graph.AddDirectedEdge(c, d, 8);
+            Graph.AddDirectedEdge(d, c, 8);
+            Graph.AddDirectedEdge(d, e, 6);
+            Graph.AddDirectedEdge(a, d, 5);
+            Graph.AddDirectedEdge(c, e, 2);
+            Graph.AddDirectedEdge(e, b, 3);
+            Graph.AddDirectedEdge(a, e, 7);
             base.BaseInitialize();
         }
 
